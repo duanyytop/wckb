50d855ecbaa7ed4fc501bed2dacffeab261a4283
duanyytop <duanyytop@gmail.com>
2020-03-27 21:08:44 +0800

    feat: add common components

---
+++ lib/components/components.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+Widget welcomeWCKB() {
+  return Padding(
+    padding: EdgeInsets.only(left: 50),
+    child: Container(
+      width: 360,
+      height: 220,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('Hello World', style: TextStyle(fontSize: 18, color: Color(0xffbababa), fontWeight: FontWeight.w600)),
+          Padding(
+              padding: EdgeInsets.only(top: 18),
+              child: Text.rich(
+                TextSpan(children: [
+                  TextSpan(text: "Welcome \nto  ", style: TextStyle(fontSize: 60, color: Colors.white)),
+                  TextSpan(
+                      text: "WCKB",
+                      style: TextStyle(
+                        color: Color(0xff3cc68a),
+                        fontSize: 60,
+                      )),
+                  TextSpan(text: "  :)", style: TextStyle(fontSize: 60, color: Colors.white)),
+                ]),
+              ))
+        ],
+      ),
+    ),
+  );
+}
+
+Widget mainPenal(Widget business) {
+  return Padding(
+    padding: EdgeInsets.only(left: 84),
+    child: Container(
+      width: 580,
+      height: 474,
+      decoration: BoxDecoration(
+        border: new Border.all(color: Colors.white, width: 1), // 边色与边宽度
+        borderRadius: new BorderRadius.only(bottomLeft: Radius.circular(82), bottomRight: Radius.circular(82)),
+      ),
+      child: business,
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:wckb/components/components.dart';
+
+var _screenWidth = 0.0;
+var _screenHeight = 0.0;
 
 void main() {
   runApp(MyApp());
@@ -9,19 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'WCKB',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'WCKB'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -30,15 +28,24 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    _screenWidth = MediaQuery.of(context).size.width;
+    _screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Text(
-          'Nervos CKB UDT WCKB',
-        ),
-      ),
-    );
+        backgroundColor: Color(0x3c3e45),
+        body: Container(
+            width: _screenWidth,
+            height: _screenHeight,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                mainPenal(Center(
+                  child: Text(
+                    'Welcome',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )),
+                welcomeWCKB()
+              ],
+            )));
   }
 }
